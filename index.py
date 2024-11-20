b1afb5ecd89158b6396bd299256b0d85923976b9
hibamallat <hibamallat040@gmail.com>
2024-11-20 19:50:39 +0200

    Add a list for test

--- index.py
+++ index.py
@@ -9,5 +9,8 @@ def avg(array):
         
         return (array[0] + (count - 1) * avg(array[1:]))/count
 
-
+#array = [5 , 5 , 5]
+#array = [10 , 20 , 30 , 40]
+array = [3, 7, 12, -5, 8, 15]
+print(avg(array))
  
